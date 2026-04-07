28b57f9634dae2d15748e12c08a92ddebca07241
Sheshan Dharmasiri <fas212212@kul.wyb.ac.lk>
2026-04-07 15:21:47 +0530

    Difference between Hard assertions and soft assertions

---
+++ tests/softAssertions.spec.js
@@ -0,0 +1,17 @@
+const {test, expect} = require('@playwright/test')
+
+test('SoftAssertions', async ({page}) =>{
+    await page.goto("https://demoblaze.com/index.html")
+
+/*    //Hard Assertion
+    await expect(page).toHaveTitle('STORE')
+    await expect(page).toHaveURL('https://demoblaze.com/index.html')
+    await expect(page.locator("#nava")).toBeVisible()
+*/
+    //soft assertion
+    await expect.soft(page).toHaveTitle('STORE')
+    await expect.soft(page).toHaveURL('https://demoblaze.com/index.html')
+    await expect.soft(page.locator("#nava")).toBeVisible()
+
+}
+)
