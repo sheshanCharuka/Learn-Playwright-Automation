927c6658e283cea7409bab9ea0a25d79dcc98582
Sheshan Dharmasiri <fas212212@kul.wyb.ac.lk>
2026-04-09 11:53:03 +0530

    Update locatingMultipleElements.spec.js

--- tests/locatingMultipleElements.spec.js
+++ tests/locatingMultipleElements.spec.js
@@ -12,7 +12,7 @@ test("LocateMultipleElements", async ({page})=>{
         console.log(linktext);  //print the text content of each link
     }
 */
-    page.waitForSelector("//div[@id='tbodyid']//div//h4")  //wait for the product list to load before locating the elements 
+    await page.waitForSelector("//div[@id='tbodyid']//div//h4")  //wait for the product list to load before locating the elements 
    
     //Product list in the page
     const products = await page.$$("//div[@id='tbodyid']//div//h4");
